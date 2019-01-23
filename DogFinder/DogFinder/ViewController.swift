161353a44e640117e851a1b80365f09abf3941c5
Kirsten Loechl <kirsten.loechl7@gmail.com>
2019-01-23 10:42:14 -0800

    Initial setup

---
+++ DogFinder/DogFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogFinder
+//
+//  Created by Kirsten M Loechl on 1/23/19.
+//  Copyright © 2019 Kirsten Loechl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
