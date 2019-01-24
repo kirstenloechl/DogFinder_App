37c5fa3a5388cb344a327b715078fe1f345796b3
Kirsten Loechl <kirsten.loechl7@gmail.com>
2019-01-24 12:56:30 -0800

    login buttons done

--- DogFinder/DogFinder/AppDelegate.swift
+++ DogFinder/DogFinder/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        //FirebaseApp.configure()
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
@@ -46,6 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+   
 
 
 }
--- DogFinder/DogFinder/ViewController.swift
+++ DogFinder/DogFinder/ViewController.swift
@@ -15,11 +15,48 @@ class ViewController: UIViewController {
         button.readPermissions = ["email"]
         return button
     }()
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var dogfinderloginbutton: UIButton!
+    @IBOutlet weak var signupbutton: UIButton!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        let purpleColor = hexStringToUIColor(hex: "#BCA5FF")
+        logo.image = #imageLiteral(resourceName: "logo.png")
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         view.addSubview(loginbutton)
-        loginbutton.center = view.center
+        loginbutton.center.x = view.center.x
+        loginbutton.center.y = 560
+        
+        dogfinderloginbutton.layer.cornerRadius = 3
+        dogfinderloginbutton.layer.borderWidth = 1
+        dogfinderloginbutton.layer.borderColor = purpleColor.cgColor
+        
+        signupbutton.layer.cornerRadius = 3
+        signupbutton.layer.borderWidth = 1
+        signupbutton.layer.borderColor = purpleColor.cgColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
